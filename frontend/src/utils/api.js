45d911c01012a2cd614a74c254a1e048447719ed
Andrej Kudinov <kudinov.andrej@list.ru>
2023-06-15 20:15:22 +0300

    исправление ошибок

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,7 +6,7 @@ class Api {
     _getHeaders() {
         return {
             "Content-Type": "application/json",
-            authorization: `Bearer ${localStorage.getItem('userToken')}`
+            authorization: `Bearer ${localStorage.getItem('token')}`
         };
     }
 
